9be8178c1f84941546597ead3a6beb73e8ffab9e
MiLee777 <midyashka1506@gmail.com>
2024-08-22 18:21:10 +0700

    feat: add burger menu

--- src/App.js
+++ src/App.js
@@ -3,8 +3,13 @@ import "./App.css";
 import { MenuPage } from "./Components/MenuPage/MenuPage";
 import Logo from "./assets/logo.png";
 import { MainPage } from "./Components/MainPage/MainPage";
+import { useState } from "react";
 
 function App() {
+
+  const [showMenu, setShowMenu] = useState(false);
+  
+
   return (
     <div className="App">
       <Router>
@@ -15,7 +20,7 @@ function App() {
                 <img src={Logo} alt="Logo" />
               </Link>
             </div>
-            <nav className="header__nav">
+            <nav className={`header__nav ${showMenu ? "show-menu" : ""}`}>
               <ul className="header__menu">
                 <li className="header__link"><a href="#" className="link">Favorite coffee</a></li>
                 <li className="header__link"><a href="#" className="link">About</a></li>
@@ -34,21 +39,28 @@ function App() {
                   <path d="M14.167 9.76667V11.6667C14.167 14.8883 11.5553 17.5 8.33366 17.5C5.112 17.5 2.50033 14.8883 2.50033 11.6667V9.76667C2.50033 9.4353 2.76896 9.16667 3.10033 9.16667H13.567C13.8984 9.16667 14.167 9.4353 14.167 9.76667Z" stroke="#403F3D" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
                   <path d="M10.0003 7.50008C10.0003 6.66675 10.5956 5.83341 11.786 5.83341V5.83341C13.101 5.83341 14.167 4.76743 14.167 3.45246V2.91675"
                     stroke="#403F3D"
-                    stroke-width="1.5"
-                    stroke-linecap="round"
-                    stroke-linejoin="round" />
+                    strokeWidth="1.5"
+                    strokeLinecap="round"
+                    strokeLinejoin="round" />
                   <path d="M6.66634 7.5V7.08333C6.66634 5.70262 7.78563 4.58333 9.16634 4.58333V4.58333C10.0868 4.58333 10.833 3.83714 10.833 2.91667V2.5"
                     stroke="#403F3D"
-                    stroke-width="1.5"
-                    stroke-linecap="round"
-                    stroke-linejoin="round" />
+                    strokeWidth="1.5"
+                    strokeLinecap="round"
+                    strokeLinejoin="round" />
                   <path d="M13.333 9.16675H15.4163C16.5669 9.16675 17.4997 10.0995 17.4997 11.2501C17.4997 12.4007 16.5669 13.3334 15.4163 13.3334H14.1663"
                     stroke="#403F3D"
-                    stroke-width="1.5"
-                    stroke-linecap="round"
-                    stroke-linejoin="round" />
+                    strokeWidth="1.5"
+                    strokeLinecap="round"
+                    strokeLinejoin="round" />
                 </svg>
               </Link>
+            </div>
+            <div className="header__hamb"
+              onClick={() => setShowMenu(!showMenu)}>
+              <div className={ `hamb__field ${showMenu ? "active" : ""}` }>
+                <span className="hamb__bar"></span>
+                <span className="hamb__bar"></span>
+              </div>
             </div>
           </div>
         </header>
